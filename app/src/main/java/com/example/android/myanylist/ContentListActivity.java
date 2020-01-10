0403793154b94dbd6796fe975a17cc38e36443e2
uuupah <uuupah@gmail.com>
2020-01-10 18:26:55 +1030

    add unversioned files

---
+++ app/src/main/java/com/example/android/myanylist/ContentListActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.myanylist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.myanylist.models.ContentItem;
+
+public class ContentListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_list);
+    }
+}
