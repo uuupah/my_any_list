ac43304d7fd3a43d65ea486171cfdd1a07c50f4a
uuupah <uuupah@gmail.com>
2020-01-14 13:38:33 +1030

    implement filling view activity with content

--- app/src/main/java/com/example/android/myanylist/ContentActivity.java
+++ app/src/main/java/com/example/android/myanylist/ContentActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,11 +16,11 @@ public class ContentActivity extends AppCompatActivity {
     private static final String TAG = "ContentActivity";
 
     // ui components
-    private ImageView mImage;
-    private TextView mViewTitle, mViewDescription, mStatus, mCreator, mDateCreated;
+    private ImageView mImageView;
+    private EditText mViewTitle, mViewDescription, mStatus, mCreator, mDateCreated;
 
     // vars
-    private static ContentItem Content;
+    private static ContentItem initialContent;
     private boolean mIsNewEntry;
 
     @Override
@@ -33,29 +34,54 @@ public class ContentActivity extends AppCompatActivity {
         mCreator = findViewById(R.id.view_creator);
         mDateCreated = findViewById(R.id.view_date_created);
 
+        mImageView = findViewById(R.id.view_image);
+
         if(getIncomingIntent()){
             // new entry, edit mode
             //TODO
+
+            // on edit:
+            // allow access to edittexts
+            // set edittext background to a colour
+            // allow modification of image
+            // swap status with a spinner for statuses
+            // make title and description header visible
         } else {
             // existing note, view mode
-            //TODO
+            updateViews();
         }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.content_list_toolbar);
         setSupportActionBar(toolbar);
-        setTitle(Content.getTitle());
+        setTitle(initialContent.getTitle());
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
     }
 
     private boolean getIncomingIntent(){
         if(getIntent().hasExtra("selected_content")) {
-            Content = getIntent().getParcelableExtra("selected_content");
-            Log.d(TAG, "onCreate: content imported: " + Content.toString());
+            initialContent = getIntent().getParcelableExtra("selected_content");
+            Log.d(TAG, "onCreate: content imported: " + initialContent.toString());
 
             mIsNewEntry = false;
             return false;
         }
         mIsNewEntry = true;
         return true;
+    }
+
+    private void updateViews(){
+        String title = initialContent.getTitle();
+        String description = initialContent.getDescription();
+        String status = ContentItem.getStringStatus(initialContent.getStatus());
+        String creator = initialContent.getCreator();
+        String dateCreated = initialContent.getDateCreated();
+        int imageRes = initialContent.getImage();
+
+        mViewTitle.setText(title);
+        mViewDescription.setText(description);
+        mStatus.setText(status);
+        mCreator.setText(creator);
+        mDateCreated.setText(dateCreated);
+        mImageView.setImageResource(imageRes);
     }
 }
