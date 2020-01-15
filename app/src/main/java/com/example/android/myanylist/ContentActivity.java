443ed08ad386875acb3ba10ba0419b0edf3736c8
uuupah <uuupah@gmail.com>
2020-01-15 20:20:15 +1030

    implement editmode basics

--- app/src/main/java/com/example/android/myanylist/ContentActivity.java
+++ app/src/main/java/com/example/android/myanylist/ContentActivity.java
@@ -5,83 +5,175 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.example.android.myanylist.models.ContentItem;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class ContentActivity extends AppCompatActivity {
+public class ContentActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "ContentActivity";
+    private static final int EDIT_MODE_ENABLED = 1;
+    private static final int EDIT_MODE_DISABLED = 0;
 
     // ui components
     private ImageView mImageView;
-    private EditText mViewTitle, mViewDescription, mStatus, mCreator, mDateCreated;
+    private ImageButton mBackView, mCheckView;
+    private EditText mViewTitle, mViewDescription, mViewStatus, mViewCreator, mViewDateCreated;
+    private FloatingActionButton mFab;
 
     // vars
-    private static ContentItem initialContent;
+    private static ContentItem mInitialContent;
     private boolean mIsNewEntry;
+    private String mTitle, mDescription, mStatus, mCreator, mDateCreated;
+    private int mImageRes, mStatusInt, mMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
 
+        mBackView = findViewById(R.id.view_back_button);
+        mCheckView = findViewById(R.id.view_check_button);
+
         mViewTitle = findViewById(R.id.view_title);
         mViewDescription = findViewById(R.id.view_description);
-        mStatus = findViewById(R.id.view_status);
-        mCreator = findViewById(R.id.view_creator);
-        mDateCreated = findViewById(R.id.view_date_created);
+        mViewStatus = findViewById(R.id.view_status);
+        mViewCreator = findViewById(R.id.view_creator);
+        mViewDateCreated = findViewById(R.id.view_date_created);
+        mFab = findViewById(R.id.content_fab);
 
         mImageView = findViewById(R.id.view_image);
 
-        if(getIncomingIntent()){
+        findViewById(R.id.content_fab).setOnClickListener(this);
+        mCheckView.setOnClickListener(this);
+
+        if (getIncomingIntent()) {
             // new entry, edit mode
+            initialiseContent();
+            enableEditMode();
+
             //TODO
 
             // on edit:
             // allow access to edittexts
             // set edittext background to a colour
+
             // allow modification of image
             // swap status with a spinner for statuses
             // make title and description header visible
         } else {
             // existing note, view mode
-            updateViews();
+            readContent();
         }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.content_list_toolbar);
         setSupportActionBar(toolbar);
-        setTitle(initialContent.getTitle());
-        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
     }
 
-    private boolean getIncomingIntent(){
-        if(getIntent().hasExtra("selected_content")) {
-            initialContent = getIntent().getParcelableExtra("selected_content");
-            Log.d(TAG, "onCreate: content imported: " + initialContent.toString());
+    private boolean getIncomingIntent() {
+        if (getIntent().hasExtra("selected_content")) {
+            mInitialContent = getIntent().getParcelableExtra("selected_content");
+            Log.d(TAG, "onCreate: content imported: " + mInitialContent.toString());
 
+            mMode = EDIT_MODE_DISABLED;
             mIsNewEntry = false;
             return false;
         }
+        mMode = EDIT_MODE_ENABLED;
         mIsNewEntry = true;
         return true;
     }
 
-    private void updateViews(){
-        String title = initialContent.getTitle();
-        String description = initialContent.getDescription();
-        String status = ContentItem.getStringStatus(initialContent.getStatus());
-        String creator = initialContent.getCreator();
-        String dateCreated = initialContent.getDateCreated();
-        int imageRes = initialContent.getImage();
+    private void enableEditMode(){
+        displayEditTextUnderline();
+        mBackView.setVisibility(View.GONE);
+        mCheckView.setVisibility(View.VISIBLE);
 
-        mViewTitle.setText(title);
-        mViewDescription.setText(description);
-        mStatus.setText(status);
-        mCreator.setText(creator);
-        mDateCreated.setText(dateCreated);
-        mImageView.setImageResource(imageRes);
+        mMode = EDIT_MODE_ENABLED;
+
+        mFab.hide();
+    }
+
+    private void disableEditMode(){
+        hideEditTextUnderline();
+        mBackView.setVisibility(View.VISIBLE);
+        mCheckView.setVisibility(View.GONE);
+
+        mMode = EDIT_MODE_DISABLED;
+
+        mFab.show();
+    }
+
+    private void readContent() {
+        mTitle = mInitialContent.getTitle();
+        mDescription = mInitialContent.getDescription();
+        mStatus = ContentItem.getStringStatus(mInitialContent.getStatus());
+        mCreator = mInitialContent.getCreator();
+        mDateCreated = mInitialContent.getDateCreated();
+        mImageRes = mInitialContent.getImage();
+
+        fillViews();
+    }
+
+    private void initialiseContent() {
+        mTitle = "New Entry";
+        mDescription = "Describe the entry here.";
+        mStatusInt = 0;
+        mStatus = ContentItem.getStringStatus(mStatusInt);
+//        creator = "Creator";
+//        dateCreated = "01 Jan 1970";
+        mImageRes = R.mipmap.ic_launcher; // temporary image
+
+        fillViews();
+    }
+
+    private void fillViews() {
+        mViewTitle.setText(mTitle);
+        mViewDescription.setText(mDescription);
+        mViewStatus.setText(mStatus);
+        mViewCreator.setText(mCreator);
+        mViewDateCreated.setText(mDateCreated);
+        mImageView.setImageResource(mImageRes);
+    }
+
+    private void displayEditTextUnderline() {
+        mViewTitle.setBackgroundTintList(getColorStateList(R.color.grey_underline));
+        mViewDescription.setBackgroundTintList(getColorStateList(R.color.grey_underline));
+        mViewStatus.setBackgroundTintList(getColorStateList(R.color.grey_underline));
+        mViewCreator.setBackgroundTintList(getColorStateList(R.color.grey_underline));
+        mViewDateCreated.setBackgroundTintList(getColorStateList(R.color.grey_underline));
+    }
+
+    private void hideEditTextUnderline() {
+        mViewTitle.setBackgroundTintList(getColorStateList(R.color.transparent_underline));
+        mViewDescription.setBackgroundTintList(getColorStateList(R.color.transparent_underline));
+        mViewStatus.setBackgroundTintList(getColorStateList(R.color.transparent_underline));
+        mViewCreator.setBackgroundTintList(getColorStateList(R.color.transparent_underline));
+        mViewDateCreated.setBackgroundTintList(getColorStateList(R.color.transparent_underline));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.content_fab:
+                enableEditMode();
+                break;
+            case R.id.view_check_button:
+                disableEditMode();
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mMode == EDIT_MODE_ENABLED) {
+            onClick(mCheckView);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
