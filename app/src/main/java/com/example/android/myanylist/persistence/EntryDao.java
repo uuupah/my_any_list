8c79d056aa4d08cc59e264c4be2004c85b29c20f
uuupah <uuupah@gmail.com>
2020-01-17 21:18:59 +1030

    implement data access object for entries table

---
+++ app/src/main/java/com/example/android/myanylist/persistence/EntryDao.java
@@ -0,0 +1,34 @@
+package com.example.android.myanylist.persistence;
+
+import android.provider.MediaStore;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.android.myanylist.models.MediaEntry;
+
+import java.util.List;
+
+@Dao
+public interface EntryDao {
+
+    @Insert
+    long[] insertEntry(MediaEntry... entries);
+
+    @Query("SELECT * FROM entries")
+    LiveData<List<MediaEntry>> getEntries();
+
+//    @Query("SELECT * FROM entries WHERE id = :id")
+//    List<Note> getNotesWithCustomQuery(int id)
+
+    @Delete
+    int delete(MediaEntry... entries);
+
+    @Update
+    int update(MediaEntry... entries);
+
+}
